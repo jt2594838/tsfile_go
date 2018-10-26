7e7fb03c6d3f9c63265e688ecfbbd4c66ef4c7c2
wangcan1225@hotmail.com <wangcan1225@hotmail.com>
2018-10-26 20:01:42 +0800

    add testcode

---
+++ src/tsfile/common/logcost/logcost.go
@@ -0,0 +1,11 @@
+package logcost
+
+var CostWriteTimes int64 = 0
+var CostNewTimes int64 = 0
+
+var CostWriteTimesTest1 int64 = 0
+var CostWriteTimesTest2 int64 = 0
+var CostWriteTimesTest3 int64 = 0
+var CostWriteTimesTest4 int64 = 0
+var CostWriteTimesTest5 int64 = 0
+var CostWriteTimesTest6 int64 = 0
